ae6b588ca10747b6c02f0a43966ac5a0709a8efa
Tolik Mateychuk <amateychuk@adyax.com>
2019-07-04 20:58:43 +0300

    Create custom entity

---
+++ web/modules/custom/news_entity/src/Form/NewsDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\news_entity\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting News entities.
+ *
+ * @ingroup news_entity
+ */
+class NewsDeleteForm extends ContentEntityDeleteForm {
+
+
+}
